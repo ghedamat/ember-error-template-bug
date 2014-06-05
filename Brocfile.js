a22ed1afa69470f1cf3c820c95be34aea24f06a2
Mattia Gheda <ghedamat@gmail.com>
2014-06-05 01:48:54 -0400

    use canary build, error template is not rendered
    even when error route is not defined

--- Brocfile.js
+++ Brocfile.js
@@ -1,9 +1,18 @@
 /* global require, module */
+var fileMover = require('broccoli-file-mover');
+var vendorTree = fileMover('vendor', {
+  files: {
+    'ember/index.js': 'ember/ember.js',
+  }
+});
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
   name: require('./package.json').name,
+  trees: {
+    vendor: vendorTree
+  },
 
   minifyCSS: {
     enabled: true,
